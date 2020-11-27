9f356b56feda31d614f980d9b19d9cc2cdff9955
Tiago Yoneda <xyonedax@gmail.com>
2020-11-27 15:13:36 -0300

    exercicio 4

---
+++ exercises/bloco_4/4.3-introducao-a-javascript-e-logica-de-programacao_3/exercicio_4.js
@@ -0,0 +1,13 @@
+/* 
+https://github.com/tiago-yoneda
+
+4- Depois, faça uma pirâmide com n asteriscos de base:
+*/
+
+n = 5;
+
+for ( let num = 0 ; num < n ; num+=1){
+    if ( (num+1)%2 == 1 ) {
+        console.log(' '.repeat(Math.floor((n-num)/2))+'*'.repeat(num+1));
+    }
+}
