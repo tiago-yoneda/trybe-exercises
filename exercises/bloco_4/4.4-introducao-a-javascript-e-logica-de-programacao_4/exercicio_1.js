443a8970e309d2914a1521ac70190b6b88820f11
Tiago Yoneda <xyonedax@gmail.com>
2020-11-30 15:59:44 -0300

    exercicios 1 e 2 bloco 4.4

---
+++ exercises/bloco_4/4.4-introducao-a-javascript-e-logica-de-programacao_4/exercicio_1.js
@@ -0,0 +1,17 @@
+/*
+https://github.com/tiago-yoneda
+
+Usando o objeto abaixo, faça os exercícios a seguir:
+
+Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome.
+Valor esperado no console: Bem-vinda, Margarida
+
+*/
+
+let info = {
+    personagem: "Margarida",
+    origem: "Pato Donald",
+    nota: "Namorada do personagem principal nos quadrinhos do Pato Donald",
+  };
+  
+  console.log('Bem vinda,', info.personagem);
