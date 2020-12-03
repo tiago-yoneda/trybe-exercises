1c5307f3086f3c07f8085c82bca12bf310ecebc9
Tiago Yoneda <xyonedax@gmail.com>
2020-12-03 19:25:31 -0300

    exercicio 1.1

---
+++ exercises/bloco_5/5.2-javascript-dom-eventos-e-web-storage_2/exercicios_5.2.js
@@ -0,0 +1,13 @@
+/* 
+https://github.com/tiago-yoneda
+
+O objetivo desses exercícios é colocar em prática o que você acabou de aprender sobre DOM. Por isso, você deve fazer os exercícios utilizando apenas código JavaScript , o qual deve ser inserido entre as tags <script> e </script> .
+Para uma melhor organização, faça commits a cada tarefa concluída. Vamos aos exercícios:
+*/
+
+// 1 - Adicione a tag h1 com o texto Exercício 5.2 - JavaScript DOM como filho da tag body ;
+let newH1 = document.createElement('h1');
+newH1.innerText = 'Exercício 5.2';
+
+let body = document.querySelector('body')
+body.appendChild(newH1);
