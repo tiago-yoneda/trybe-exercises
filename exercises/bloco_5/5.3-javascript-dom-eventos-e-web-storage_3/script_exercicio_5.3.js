0b35ea17f22b1482d397b620d5fc23ef9f784b54
Tiago Yoneda <xyonedax@gmail.com>
2020-12-04 18:19:35 -0300

    exercicio 8

--- exercises/bloco_5/5.3-javascript-dom-eventos-e-web-storage_3/script_exercicio_5.3.js
+++ exercises/bloco_5/5.3-javascript-dom-eventos-e-web-storage_3/script_exercicio_5.3.js
@@ -174,3 +174,21 @@ function createTask (string){
 }
 
 createTask('Cozinhar');
+
+/* Exercicio 8
+
+Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+*/
+
+function adicionaLagenda (corString){
+  let legenda = document.createElement('div');
+  legenda.className = 'task';
+  legenda.style.backgroundColor = corString;
+  document.querySelector('.my-tasks').appendChild(legenda);
+}
+
+adicionaLagenda('#cbc0d3');
+
