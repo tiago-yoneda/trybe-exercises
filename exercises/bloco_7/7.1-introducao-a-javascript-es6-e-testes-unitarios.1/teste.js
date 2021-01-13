a258d6058a94b0ac56704141d08a6a6c14b436d0
tiago-yoneda <xyonedax@gmail.com>
2021-01-13 18:16:44 -0300

    renomeando

--- exercises/bloco_7/7.1-introducao-a-javascript-es6-e-testes-unitarios.1/teste.js
+++
@@ -1,9 +0,0 @@
-const fatorial = inputFatorial => {
-  let calcFatorial = 1;
-  for (let i = inputFatorial; i != 0; i -= 1 ){
-      calcFatorial = calcFatorial * i;
-  }
-  console.log(calcFatorial);
-}
-
-fatorial(4);
