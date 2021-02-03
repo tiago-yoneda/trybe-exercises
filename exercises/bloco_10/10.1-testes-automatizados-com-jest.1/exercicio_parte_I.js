5b94261a0581247ca2d3428a6bb3ca5355de9592
tiago-yoneda <xyonedax@gmail.com>
2021-02-03 19:23:27 -0300

    limpando a pasta

--- exercises/bloco_10/10.1-testes-automatizados-com-jest.1/exercicio_parte_I.js
+++
@@ -1,8 +0,0 @@
-const sum = (a, b) => {
-  if (typeof a  !== 'number' || typeof b !== 'number') {
-    throw new Error('Parameters must be Numbers');
-  }
-  return (a + b);
-};
-
-module.exports = sum;
