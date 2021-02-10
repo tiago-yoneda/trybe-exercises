00649c243d21fd5041c268e176ba0df5f6700a1d
tiago-yoneda <xyonedax@gmail.com>
2021-02-10 19:31:19 -0300

    finalizando exercicios

---
+++ exercises/modulo_2_-_desenvolvimento_front-end/bloco_11/11.1-introducao-a-react.1/exercicios_11.1/src/HelloWorld.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+class HelloWorld extends Component {
+  render() {
+    return (
+    <div>
+      <h1 className='fullName'>Tiago Yoneda</h1>
+      <p className='introParagraph'>Estudante de Full stack na trybe. Meus hobbies são Jogar Monster Hunter e ler light novels.</p>
+    </div>
+    )
+  }
+}
+
+export default HelloWorld;
